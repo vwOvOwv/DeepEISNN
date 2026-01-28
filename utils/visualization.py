refactor: remove gradient visualization and fixing directory naming

utils/visualization.py
@@ -1,4 +1,3 @@
-import time
 import os
 import matplotlib.pyplot as plt
 import matplotlib
@@ -16,10 +15,8 @@ from typing import Any, Iterator
 class Visualizer:
     def __init__(self, output_dir: str) -> None:
         self.output_dir = output_dir
-        self.tmp_states_dir = os.path.join(self.output_dir, 'tmp_states')
-        self.tmp_gradients_dir = os.path.join(self.output_dir, 'tmp_gradients')
+        self.tmp_states_dir = os.path.join(self.output_dir, 'tmp-states')
         os.makedirs(self.tmp_states_dir, exist_ok=True)
-        os.makedirs(self.tmp_gradients_dir, exist_ok=True)
 
         self.save_queue = Queue()
         self._shutdown_flag = False
@@ -92,13 +89,6 @@ class Visualizer:
     
     def visualize_model_states(self, model: torch.nn.Module, 
                                epoch: int, iteration: int) -> None:
-        """
-        Visualize the current model state by plotting histograms of weights and activations.
-        Args:
-            model (torch.nn.Module): The model whose states are to be visualized.
-            epoch (int): The current epoch number.
-            iteration (int): The current iteration number.
-        """
         with torch.no_grad():
             layer_idx = 0
             for layer in model.layers:
@@ -108,102 +98,7 @@ class Visualizer:
                         self._plot_hist(layer._get_name(), layer_idx, val, name, epoch, iteration)
                     layer.visualize_cache.clear()
 
-    def visualize_grad_flow(self, named_parameters, epoch: int, iteration: int):
-        '''
-        Plots the gradients flowing through different layers in the net during training.
-        Highlights W_EI gradients and separates layers visually.
-        '''
-        param_rename_map = {
-            "alpha": r"$g_I$",
-            "gain": r"$g_E$",
-            "conv_ee": r"$W_{EE}$",
-            "conv_ie": r"$W_{IE}$",
-            "weight_ei": r"$W_{EI}$",
-        }
-        ave_grads = []
-        layers = []
-        colors = []
-        
-        highlight_color = '#d62728'
-        normal_color = '#1f77b4'
-        bar_alpha = 0.8
-
-        for n, p in named_parameters:
-            if p.grad is None:
-                continue
-            
-            # 过滤掉 bias，只看 weight 以保持图表整洁
-            if p.requires_grad and ("bias" not in n):
-                short_name = n.replace("module.", "")
-                for key, val in param_rename_map.items():
-                    if key in short_name:
-                        short_name = param_rename_map[key]
-                        break
-                if 'layers' in short_name:
-                    break
-                layers.append(short_name)
-                
-                ave_grad = p.grad.abs().mean().item()
-                ave_grads.append(ave_grad)
-                
-                if "weight_ei" in n.lower(): 
-                    colors.append(highlight_color)
-                else:
-                    colors.append(normal_color)
-
-        plt.rcParams.update({'font.size': 14})
-        fig, ax = plt.subplots(figsize=(14, 7))
-
-        x_pos = np.arange(len(ave_grads))
-        bars = ax.bar(x_pos, ave_grads, alpha=bar_alpha, width=0.6, color=colors, align='center')
-
-        prev_layer_idx = 1
-        for i, name in enumerate(layers):
-            if 'layer' in name:
-                break
-            if prev_layer_idx != -1 and name == r"$W_{EE}$":
-                ax.axvline(x=i - 0.5, color='gray', linestyle='--', alpha=0.5, linewidth=1)
-                prev_layer_idx += 1
-
-        ax.set_ylabel("Average Gradient Magnitude", fontsize=16, labelpad=10)
-        ax.set_xlabel("Parameters", fontsize=16, labelpad=10)
-        ax.set_title(f"Gradient Magnitude at the Beginning of Training", fontsize=18, pad=15)
-        
-        
-        ax.set_xticks(x_pos)
-        ax.set_xticklabels(layers, ha="center", fontsize=14)
-        
-        ax.set_ylim(bottom=0)
-        
-        ax.grid(True, axis='y', linestyle=':', alpha=0.6)
-
-        legend_elements = [
-            Line2D([0], [0], color=normal_color, lw=6, label='$W_{EE}, W_{IE}$, $g_I$, $g_E$'),
-            Line2D([0], [0], color=highlight_color, lw=6, label='$W_{EI}$')
-        ]
-        ax.legend(handles=legend_elements, loc='upper right', fontsize=14, frameon=True)
-
-        plt.tight_layout()
-
-        buf = io.BytesIO()
-        fig.savefig(buf, format='pdf', dpi=300)
-        buf.seek(0)
-        image_data = buf.getvalue()
-        buf.close()
-
-        data_dir = os.path.join(self.tmp_gradients_dir, f'gradients')
-        if not os.path.exists(data_dir):
-            os.makedirs(data_dir)
-        file_name = f'epoch{epoch:04d}_iter{iteration:07d}.pdf'
-        image_path = os.path.join(data_dir, file_name)
-        
-        with open(image_path, 'wb') as f:
-            f.write(image_data)
-
-        plt.close(fig)
-
     def png2mp4(self):
-        """Convert cached PNG images to MP4 videos."""
         print("Converting cached images to MP4 videos...")
 
         # make sure all images are saved
@@ -242,7 +137,7 @@ class Visualizer:
                                 try:
                                     image_path = os.path.join(layer_path, image)
                                     image_array = imageio.imread(image_path)
-                                    video.append_data(image_array)  # type: ignore
+                                    video.append_data(image_array)
                                 except Exception as e:
                                     print(f"Error processing image {image}: {e}")
                                     continue
@@ -256,5 +151,4 @@ class Visualizer:
 
         # create videos from states and gradients
         _create_video_from_images(self.tmp_states_dir)
-        _create_video_from_images(self.tmp_gradients_dir)
         print("Video generation completed.")